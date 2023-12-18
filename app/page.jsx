81d283864e974925870f7ab102e9a2a5c8506111
dinesh sadhasivam <dinesh.sadhasivam@dreamguystech.com>
2023-12-18 12:02:27 +0530

    changes

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import PostsList from "../components/postsList";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <PostsList />
+    </main>
+  )
+}
